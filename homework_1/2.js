65e8cbefb7eb9368046f7dc2497481e39719e0dd
Zheshard <antonzestkov489@gmail.com>
2024-03-15 19:08:33 +0300

    task 2 complete

--- homework_1/2.js
+++ homework_1/2.js
@@ -1,1 +1,24 @@
 // 2. Напишите функцию createCounter, которая создает счетчик и возвращает объект с двумя методами: increment и decrement. Метод increment должен увеличивать значение счетчика на 1, а метод decrement должен уменьшать значение счетчика на 1. Значение счетчика должно быть доступно только через методы объекта, а не напрямую.
+
+function createCounter() {
+  let counter = 0;
+
+  return {
+    increment() {
+      counter++;
+    },
+    decrement() {
+      counter--;
+    },
+    getCounter() {
+      return counter;
+    },
+  };
+}
+
+const counter = createCounter();
+counter.increment();
+counter.increment();
+counter.increment();
+counter.decrement();
+console.log(counter.getCounter());
