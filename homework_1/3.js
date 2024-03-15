36e124cede576d9802b9cffd28115a496a2ba8ff
Zheshard <antonzestkov489@gmail.com>
2024-03-15 19:44:38 +0300

    task 3 complete

--- homework_1/3.js
+++ homework_1/3.js
@@ -1,5 +1,27 @@
+"use strict";
 // 3. Напишите рекурсивную функцию findElementByClass, которая принимает корневой элемент дерева DOM и название класса в качестве аргументов и возвращает первый найденный элемент с указанным классом в этом дереве.
 //    Пример
 //    const rootElement = document.getElementById('root');
 //    const targetElement = findElementByClass(rootElement, 'my-class');
 //    console.log(targetElement);
+
+const rootElement = document.getElementById("root");
+
+function findElementByClass(root, className) {
+  if (root.classList && root.classList.contains(className)) {
+    return root;
+  }
+
+  let children = root.children;
+  for (let i = 0; i < children.length; i++) {
+    let foundElement = findElementByClass(children[i], className);
+    if (foundElement) {
+      return foundElement;
+    }
+  }
+
+  return null;
+}
+
+const targetElement = findElementByClass(rootElement, "my-class");
+console.log(targetElement);
