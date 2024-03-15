aeea3b4f3572086789987a0ce708960757bb8138
Zheshard <antonzestkov489@gmail.com>
2024-03-15 18:24:22 +0300

    homework 1

---
+++ homework_1/3.js
@@ -0,0 +1,5 @@
+// 3. Напишите рекурсивную функцию findElementByClass, которая принимает корневой элемент дерева DOM и название класса в качестве аргументов и возвращает первый найденный элемент с указанным классом в этом дереве.
+//    Пример
+//    const rootElement = document.getElementById('root');
+//    const targetElement = findElementByClass(rootElement, 'my-class');
+//    console.log(targetElement);
