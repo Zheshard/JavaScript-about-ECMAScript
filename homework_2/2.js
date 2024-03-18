f85fde0a75dda5a5957993362a250c54181b00cb
Zheshard <antonzestkov489@gmail.com>
2024-03-18 13:26:22 +0300

    task 2 complete

--- homework_2/2.js
+++ homework_2/2.js
@@ -6,9 +6,6 @@
 // Свойство age (возраст) - число, возраст студента.
 // Свойство grade (класс) - строка, класс, в котором учится студент.
 // Метод displayInfo() - выводит информацию о студенте (имя, возраст и класс).
-// javascript
-
-// ---
 
 // // Пример использования класса
 // const student1 = new Student(""John Smith"", 16, ""10th grade"");
@@ -24,3 +21,20 @@
 // // Name: Jane Doe
 // // Age: 17
 // // Grade: 11th grade"
+
+class Student {
+  constructor(name, age, grade) {
+    this.name = name;
+    this.age = age;
+    this.grade = grade;
+  }
+
+  displayInfo() {
+    console.log(`Name: ${this.name}\nAge: ${this.age}\nGrade: ${this.grade}`);
+  }
+}
+
+const student1 = new Student("John Smith", 16, "10th grade");
+student1.displayInfo();
+const student2 = new Student("Jane Doe", 17, "11th grade");
+student2.displayInfo();
