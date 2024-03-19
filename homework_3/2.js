a4f5d25b12972c7818cb67a08b273dc0bec0ab62
Zheshard <antonzestkov489@gmail.com>
2024-03-19 08:46:11 +0300

    task 2 complete

--- homework_3/2.js
+++ homework_3/2.js
@@ -26,3 +26,37 @@
 // order.addProduct(product2);
 
 // console.log(order.getTotalPrice()); // Вывод: 600
+
+class Product {
+  constructor(name, price) {
+    this.name = name;
+    this.price = price;
+  }
+}
+
+class Order {
+  constructor(orderNumber) {
+    this.orderNumber = orderNumber;
+    this.products = [];
+    this.totalCost = 0;
+  }
+
+  addProduct(product) {
+    this.products.push(product);
+    this.totalCost += product.price;
+  }
+
+  getTotalPrice() {
+    return this.totalCost;
+  }
+}
+
+const order = new Order(12345);
+
+const product1 = new Product("Phone", 500);
+order.addProduct(product1);
+
+const product2 = new Product("Headphones", 100);
+order.addProduct(product2);
+
+console.log(order.getTotalPrice()); // Вывод: 600
