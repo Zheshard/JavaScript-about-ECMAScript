56ebca9bf7fe15f0b00298548c01f14d44a98ce2
Zheshard <antonzestkov489@gmail.com>
2024-03-19 08:36:30 +0300

    task 1 complete

--- homework_3/1.js
+++ homework_3/1.js
@@ -22,3 +22,35 @@
 // // Вывод:
 // // Name: Jane Doe
 // // Department: Sales
+
+class Employee {
+  constructor(name) {
+    this.name = name;
+  }
+
+  displayInfo() {
+    console.log(`Name: ${this.name}`);
+  }
+}
+
+class Manager extends Employee {
+  constructor(name, department) {
+    super(name);
+    this.department = department;
+  }
+
+  displayInfo() {
+    console.log(`Name: ${this.name}\nDepartment: ${this.department}`);
+  }
+}
+
+const employee = new Employee("John Smith");
+employee.displayInfo();
+// Вывод:
+// Name: John Smith
+
+const manager = new Manager("Jane Doe", "Sales");
+manager.displayInfo();
+// Вывод:
+// Name: Jane Doe
+// Department: Sales
