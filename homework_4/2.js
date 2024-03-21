da314886b1a3baa914dd2541b75b8ade32413b85
Zheshard <antonzestkov489@gmail.com>
2024-03-21 14:08:08 +0300

    task 2 complete

--- homework_4/2.js
+++ homework_4/2.js
@@ -23,3 +23,39 @@
 // });
 
 // saveUserData использует fetch для отправки данных о пользователе на удаленный сервер для сохранения. Она отправляет POST-запрос на URL-адрес /users с указанием типа содержимого application/json и сериализует объект с данными о пользователе в JSON-строку с помощью JSON.stringify(). Если запрос успешен (с кодом 200), функция разрешает промис. Если запрос неуспешен, функция отклоняет промис с сообщени
+
+const user = {
+    name: "John Smith",
+    age: 30,
+    email: "john@example.com",
+};
+
+saveUserData(user)
+    .then(() => {
+        console.log("User data saved successfully");
+    })
+    .catch((error) => {
+        console.log(error.message);
+    });
+
+function saveUserData(userData) {
+    return new Promise((resolve, reject) => {
+        fetch("https://jsonplaceholder.typicode.com/users", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(userData),
+        })
+            .then((response) => {
+                if (response.ok) {
+                    resolve();
+                } else {
+                    reject(new Error("Error saving user data"));
+                }
+            })
+            .catch((error) => {
+                reject(new Error("Error saving user data"));
+            });
+    });
+}
